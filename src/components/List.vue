0ee67c0462ae537cdf59d607496e93c181d8276e
Eluwing <abek0403@gmail.com>
2024-08-19 17:54:36 +0900

    [Feature]ListコンポーネントCSS改善

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,32 +1,32 @@
 <template>
-  <div v-for="(item, index) in blogPosts" :key="index">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img
-              src="https://via.placeholder.com/350x150"
-              class="card-img-top"
-              alt="Post 1"
-            />
-            <div class="card-body">
-              <h5 class="card-title">
-                {{ item.title }}
-              </h5>
-              <p class="card-text">
-                {{ item.content }}
-              </p>
-              <a :href="`/detail/${item.number}`" class="btn btn-primary">Read More</a>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4" v-for="(item, index) in blogPosts" :key="index">
+        <div class="card mb-4 shadow-sm">
+          <img
+            src="https://via.placeholder.com/350x150"
+            class="card-img-top"
+            alt="Post 1"
+          />
+          <div class="card-body">
+            <h5 class="card-title">
+              {{ item.title }}
+            </h5>
+            <p class="card-text">
+              {{ item.content }}
+            </p>
+            <a :href="`/detail/${item.number}`" class="btn btn-primary"
+              >Read More</a
+            >
+          </div>
+          <div class="card-footer text-muted">
+            {{ item.date }}
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-<!-- <router-link :to="`/detail/${item.number}`">{{
-  item.title
-}}</router-link> -->
 <script>
 export default {
   name: "ListItem",
